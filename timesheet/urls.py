6203bd9a249f5ad55ddc10ef3f7fba8c71ea5b7a
Leandro Hinckel Silveira <leandro.hinckel@gmail.com>
2016-03-12 21:36:04 -0300

    Adicionada novas Views para entidade Usuario

--- timesheet/urls.py
+++ timesheet/urls.py
@@ -3,5 +3,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.ListaUsuariosView.as_view(), name='lista_usuarios'),
+	url(r'^usuario/novo$', views.novo_usario, name='novo_usuario'),
+	url(r'^usuario/salvar$', views.salvar_usuario, name='salvar_usuario'),
+	url(r'^usuario/(?P<pk>[0-9]+)/$', views.VisualizarUsuarioView.as_view(), name='visualizar_usuario'),
 ]
--- timesheet/views.py
+++ timesheet/views.py
@@ -1,6 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from django.shortcuts import get_object_or_404, render
+from .models import Usuario, Pessoa, Documento, TipoDocumento
+from django.views import generic
 
 # Create your views here.
-def index(request):
-	return HttpResponse("Hello World")
+class ListaUsuariosView(generic.ListView):
+    context_object_name = 'usuarios'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Usuario.objects.order_by('-data_criacao')[:5]
+
+class VisualizarUsuarioView(generic.DetailView):
+	model=Usuario
+	context_object_name="usuario"
+
+def novo_usario(request):
+	usuario = Usuario()
+	usuario.pessoa = Pessoa()
+	usuario.pessoa.documento = Documento()
+	tipos_documentos = TipoDocumento.objects.all()
+	usuario.pessoa.documento.tipo_documento = tipos_documentos[0]
+	return render(request, 'timesheet/usuario_form.html', {'usuario': usuario, 'tipos_documentos': tipos_documentos, 'method': 'post'})
+
+def salvar_usuario(request):
+	pass
